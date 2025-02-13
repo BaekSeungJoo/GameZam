be546e77f54074c82cc4661db102cadca8857f7d
Foodie_Pudding <sacar1920@gmail.com>
2025-02-14 01:52:46 +0900

    데드존 추가
    그럼죽어

--- InternWarrior/Assets/__Main/Scripts/DeadZone.cs
+++ InternWarrior/Assets/__Main/Scripts/DeadZone.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DeadZone : MonoBehaviour
 {
-
     private GameManager gameManager;
+
+    private void Start() {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player"))
         {
+            gameManager.MoveScene_GameEnd();
         }
     }
 }
