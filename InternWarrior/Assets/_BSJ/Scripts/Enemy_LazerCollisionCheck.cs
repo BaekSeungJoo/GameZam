9ca467403e3578081db4f215e305449b911be7e1
Foodie_Pudding <sacar1920@gmail.com>
2025-02-14 00:10:57 +0900

    문워크, 레이저 데미지 fix
    제목과 같음

--- InternWarrior/Assets/_BSJ/Scripts/Enemy_LazerCollisionCheck.cs
+++ InternWarrior/Assets/_BSJ/Scripts/Enemy_LazerCollisionCheck.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class Enemy_LazerCollisionCheck : MonoBehaviour
 {
+    [Header("레이저 데미지")]
+    public int damage = 3;
+
+    PlayerManager manager;
+
+    void Start()
+    {
+        manager = GameObject.Find("PlayerManager").GetComponent<PlayerManager>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if(collision.gameObject.CompareTag("Player") && !collision.isTrigger)
         {
             // TODO 체력 감소 로직
             print("플레이어 체력 - 3");
+
+            // 체력 감소
+            manager.Damage(damage);
+            manager.InitPlayUI();
         }
     }
 }
